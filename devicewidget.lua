7281d66a8d2018f84b949d67c22547f9dfeda064
davlord <vlord@free.fr>
2020-01-10 22:53:45 +0100

    toggle between ip4/ip6 on click

--- devicewidget.lua
+++ devicewidget.lua
@@ -1,4 +1,5 @@
 local setmetatable = setmetatable
+local next = next
 local wibox = require("wibox")
 local awful = require("awful")
 local iconhelper = require("awesome-networkmanager-widget.iconhelper")
@@ -6,17 +7,38 @@ local devicehelper = require("awesome-networkmanager-widget.devicehelper")
 
 local devicewidget = { mt = {}}
 
+devicewidget.MODE_IP4 = 4
+devicewidget.MODE_IP6 = 6
+
 local function visibility_filter(device)
     return device:get_state() == "ACTIVATED"
 end
 
+function devicewidget:set_mode(mode)
+    self._private.mode = mode
+end
+
+function devicewidget:get_device_ips(device)
+    if self._private.mode == devicewidget.MODE_IP4 then return devicehelper.get_device_ip4(device)
+    elseif self._private.mode == devicewidget.MODE_IP6 then return devicehelper.get_device_ip6(device)
+    else return {}
+    end
+end
+
 function devicewidget:update_widget(device)
-    local ips4 = devicehelper.get_device_ip4(device)
+    local ips = self:get_device_ips(device)
     local icon = iconhelper.get_device_icon(device)
     local is_visible = visibility_filter(device)    
 
+    local ip_text = ""
+    if next(ips) == nil then
+        ip_text = string.format("no ip%s address", self._private.mode)
+    else
+        ip_text = table.concat(ips,", ")
+    end
+
     self.imagebox.image = icon:load_surface()
-    self.textbox.text = table.concat(ips4,",")
+    self.textbox.text = ip_text
     self:set_visible(is_visible)
 end
 
@@ -34,7 +56,7 @@ function devicewidget:update(device)
     self:update_tooltip(device)
 end
 
-local function new()
+local function new(args)
     local w = wibox.widget {
         layout = wibox.layout.fixed.horizontal,
         spacing = 2,
@@ -49,6 +71,8 @@ local function new()
         }        
     }
     w.tooltip = awful.tooltip({ objects = { w },})
+
+    w._private.mode = args and args.mode or devicewidget.MODE_IP4
 
     setmetatable(w, {__index = devicewidget})
 
--- init.lua
+++ init.lua
@@ -1,8 +1,10 @@
+local setmetatable = setmetatable
 local lgi = require 'lgi'
 local GLib = lgi.GLib
 local NM = lgi.NM
 local awful = require("awful")
 local wibox = require("wibox")
+local gears = require("gears")
 local devicewidget = require("awesome-networkmanager-widget.devicewidget")
 local devicehelper = require("awesome-networkmanager-widget.devicehelper")
 
@@ -22,10 +24,21 @@ local function foreach_device(fn)
 	end
 end
 
+function network:toggle_mode()
+    if self._private.mode == devicewidget.MODE_IP4 then self._private.mode = devicewidget.MODE_IP6
+    elseif self._private.mode == devicewidget.MODE_IP6 then self._private.mode = devicewidget.MODE_IP4
+    end
+    foreach_device(function(device)
+        local device_widget = self:get_or_create_device_widget(device)
+        device_widget:set_mode(self._private.mode)
+        device_widget:update(device)
+    end)
+end
+
 function network:get_or_create_device_widget(device)
     -- cache widget by name
     local device_name = devicehelper.get_device_name(device)
-    self._private[device_name] = self._private[device_name] or devicewidget()
+    self._private[device_name] = self._private[device_name] or devicewidget({ mode = self._private.mode})
 
     return self._private[device_name]
 end
@@ -43,27 +56,37 @@ function network:init(nm_new_res)
     -- finalize network-manager connexion
     nm = NM.Client.new_finish(nm_new_res)
 
-    -- add listeners
+    -- add listeners on network manager events
     local update = function() self:update() end
-    function nm:on_active_connection_added(connection) 
+
+    for _, connection in ipairs(nm:get_active_connections()) do
+        connection.on_state_changed = update
+    end
+    nm.on_active_connection_added = function(_, connection)
         connection.on_state_changed = update
     end
     nm.on_active_connection_removed = update
     nm.on_device_added = update
     nm.on_device_removed = update
-   
+
+    -- add listeners on widget events
+    self:buttons(gears.table.join(
+        awful.button({ }, 1, function() self:toggle_mode() end)
+    ))
+
     -- update widget now
     self:update()
 end
 
-local function new()
+local function new(args)
     local container = wibox.layout.fixed.horizontal()
     container.spacing = 2
+    container._private.mode = args and args.mode or devicewidget.MODE_IP4
 
     setmetatable(container, {__index = network})
 
     NM.Client.new_async(nil, function(src, res) container:init(res) end)    
-
+    
     return container
 end
 
